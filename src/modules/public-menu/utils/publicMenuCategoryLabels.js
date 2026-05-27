da1ee0cb33166fe3773f0388e6a46d713506389c
A-matute <darril.matute@unah.hn>
2026-05-26 20:03:06 -0600

    Mejorando vistas y ajustes del menú publico en general.

--- src/modules/public-menu/utils/publicMenuCategoryLabels.js
+++ src/modules/public-menu/utils/publicMenuCategoryLabels.js
@@ -1,5 +1,7 @@
 export const formatPublicMenuCategoryLabel = (category) =>
   String(category || '')
+    .replace(/\brefrescos\s*\/\s*agua\b/gi, 'Refrescos - Agua')
+    .replace(/\bjugos\s+naturales?\b/gi, 'Jugos Naturales')
     .replace(/\bbirria\b/gi, 'Birria')
     .replace(/\bdogs\b/gi, 'Dogs')
     .replace(/\btenders\b/gi, 'Tenders');
